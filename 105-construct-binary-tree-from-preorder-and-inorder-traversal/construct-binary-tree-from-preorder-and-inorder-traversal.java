f7d7a39e0a792d0e9d5fe4c43c720ca196b8398a
Arjit Sharma <142593572+Arjit74@users.noreply.github.com>
2025-08-13 10:54:06 +0530

    Time: 3 ms (33.73%) | Memory: 44.4 MB (51.97%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,20 +1,40 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int idx = 0;
-    Map<Integer, Integer> map = new HashMap<>();
-
-    public TreeNode buildTree(int[] pre, int[] in) {
-        for (int i = 0; i < in.length; i++) map.put(in[i], i);
-        return build(pre, 0, in.length - 1);
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return createTree(inorder, preorder, 0, inorder.length - 1, 0, preorder.length - 1);
     }
 
-    TreeNode build(int[] pre, int l, int r) {
-        if (l > r) return null;
-        TreeNode root = new TreeNode(pre[idx++]);
-        int m = map.get(root.val);
-        root.left = build(pre, l, m - 1);
-        root.right = build(pre, m + 1, r);
-        return root;
+    public TreeNode createTree(int[] in, int[] pre, int ilo, int ihi, int plo, int phi) {
+        if (ilo > ihi || plo > phi)
+            return null;
+        TreeNode node = new TreeNode(pre[plo]);
+        int idx = search(in, ilo, ihi, pre[plo]);
+        int c = idx - ilo;
+        node.left = createTree(in, pre, ilo, idx - 1, plo + 1, plo + c);
+        node.right = createTree(in, pre, idx + 1, ihi, plo + c + 1, phi);
+        return node;
+    }
+
+    public int search(int[] in, int ilo, int ihi, int item) {
+        for (int i = ilo; i <= ihi; i++) {
+            if (in[i] == item) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
